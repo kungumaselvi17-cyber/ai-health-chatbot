cbb2c9fbd66f1875aad6fed819b916d16c863938
kungumaselvi17-cyber <kungumaselvi17@gmail.com>
2026-03-18 12:26:30 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv("model/health_data.csv")
+
 def get_ai_response(message):
 
     msg = message.lower()
 
-    if "fever" in msg:
-        return "You may take rest and stay hydrated. Consult doctor if persistent."
+    for i,row in data.iterrows():
+        if row["symptom"] in msg:
+            return f"Possible condition: {row['disease']}. Advice: {row['advice']}"
 
-    elif "headache" in msg:
-        return "Reduce screen time and drink water."
-
-    elif "cold" in msg:
-        return "Warm fluids and rest can help relieve symptoms."
-
-    elif "hello" in msg:
-        return "Hello! I am your AI Health Assistant."
-
-    return "Please consult a healthcare professional for medical advice."
+    return "Please consult a healthcare professional."
